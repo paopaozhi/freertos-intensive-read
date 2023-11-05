a269c2f4d195bc813d54fe48a9aa472caecf10ea
泡泡纸 <82105673+paopaozhi@users.noreply.github.com>
2023-11-05 11:15:56 +0800

    feat:添加数码管设备支持 (#2)
    1. 实现输入字符串

---
+++ Drivers/LedSmg/led_smg.h
@@ -0,0 +1,17 @@
+//
+// Created by paopa on 2023/5/24.
+//
+
+#ifndef __LED_SMG_H
+#define __LED_SMG_H
+
+#include "main.h"
+
+void LED_SMG_Init(void);
+
+void LED_Handle(void);
+
+void LED_Str(char *s);
+
+
+#endif //__LED_SMG_H
