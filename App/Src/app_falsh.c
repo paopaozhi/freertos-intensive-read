f2c59b737da39878d6adae0fac928b0d1627ea4b
paopaozhi <paopaozhi@hotmail.com>
2023-11-13 11:05:54 +0800

    feat:增加RTC时间储存
    1. 使用BKP备份寄存器
    2. 保存格式为RTC_FORMAT_BIN

--- App/Src/app_falsh.c
+++ App/Src/app_falsh.c
@@ -8,6 +8,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+static void WriteData(rtcConfigTypedef *rtc);
+
 uint8_t i = 1;
 void StartFalshTask(void *argument) {
     Norflash_Init();
@@ -16,6 +18,18 @@ void StartFalshTask(void *argument) {
     Norflash_Write(&i,0x01,1);
     printf("i = %d\r\n",i);
     for (;;) {
+//        WriteData();
         vTaskDelay(1);
     }
+}
+
+void WriteData(rtcConfigTypedef *rtc){
+    Norflash_Write(&rtc->data.Year,0x05,1);
+    Norflash_Write(&rtc->data.Month,0x06,1);
+    Norflash_Write(&rtc->data.Date,0x07,1);
+    Norflash_Write(&rtc->data.WeekDay,0x08,1);
+
+    Norflash_Write(&rtc->time.Hours,0x09,1);
+    Norflash_Write(&rtc->time.Minutes,0x0A,1);
+    Norflash_Write(&rtc->time.Seconds,0x0B,1);
 }
--- App/Src/app_rtc.c
+++ App/Src/app_rtc.c
@@ -23,6 +23,9 @@ static void StartRtcConfigTask(void *argument);
 
 static void RTC_Config(RTC_DateTypeDef *date, RTC_TimeTypeDef *time);
 
+static void RTC_InitSetDate(void);
+static void RTC_SaveData(rtcConfigTypedef *data);
+
 void StartRtcTask(void *argument) {
     rtcQueueHandle = xQueueCreate(5, sizeof(rtcConfigTypedef *));
     if (rtcQueueHandle == NULL) {
@@ -54,6 +57,7 @@ void StartRtcTask(void *argument) {
 rtcConfigTypedef *sConfig;
 
 static void StartRtcConfigTask(void *argument) {
+    RTC_InitSetDate();
     for (;;) {
         xQueueReceive(rtcQueueHandle, (void *) &sConfig, portMAX_DELAY);
         RTC_Config(&sConfig->data, &sConfig->time);
@@ -63,10 +67,24 @@ static void StartRtcConfigTask(void *argument) {
 static RTC_DateTypeDef sdatestructureget;
 static RTC_TimeTypeDef stimestructureget;
 
+void RTC_SaveDate(RTC_DateTypeDef *date) {
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR1, date->Year);
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR2, date->Month);
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR3, date->Date);
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR4, date->WeekDay);
+}
+
+void RTC_SaveTime(RTC_TimeTypeDef *time) {
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR5, time->Hours);
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR6, time->Minutes);
+    HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR7, time->Seconds);
+}
+
 static void RTC_TimeShow(uint8_t *showtime) {
     /* Get the RTC current Time */
     HAL_RTC_GetTime(&hrtc, &stimestructureget, RTC_FORMAT_BIN);
 
+    RTC_SaveTime(&stimestructureget);
 
     /* Display time Format : hh:mm:ss */
     sprintf((char *) showtime, "%02d-%02d-%02d", stimestructureget.Hours, stimestructureget.Minutes,
@@ -77,6 +95,8 @@ static void RTC_DataShow(uint8_t *showdata) {
     /* Get the RTC current Date */
     HAL_RTC_GetDate(&hrtc, &sdatestructureget, RTC_FORMAT_BIN);
 
+    RTC_SaveDate(&sdatestructureget);
+
     /* Display time Format : hh:mm:ss */
     sprintf((char *) showdata, "%02d-%02d-%02d", sdatestructureget.Year, sdatestructureget.Month,
             sdatestructureget.Date);
@@ -85,4 +105,26 @@ static void RTC_DataShow(uint8_t *showdata) {
 static void RTC_Config(RTC_DateTypeDef *date, RTC_TimeTypeDef *time) {
     HAL_RTC_SetTime(&hrtc, time, RTC_FORMAT_BCD);
     HAL_RTC_SetDate(&hrtc, date, RTC_FORMAT_BCD);
+}
+
+
+
+static void RTC_InitSetDate(void) {
+    rtcConfigTypedef *pConfigData;
+    pConfigData = (rtcConfigTypedef *) pvPortMalloc(sizeof(rtcConfigTypedef));
+
+    pConfigData->data.Year = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR1);
+    pConfigData->data.Month = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR2);
+    pConfigData->data.Date = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR3);
+    pConfigData->data.WeekDay = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR4);
+
+    pConfigData->time.Hours = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR5);
+    pConfigData->time.Minutes = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR6);
+    pConfigData->time.Seconds = HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR7);
+
+//    RTC_Config(&pConfigData->data, &pConfigData->time);
+    HAL_RTC_SetTime(&hrtc, &pConfigData->time, RTC_FORMAT_BIN);
+    HAL_RTC_SetDate(&hrtc, &pConfigData->data, RTC_FORMAT_BIN);
+
+    vPortFree(pConfigData);
 }
